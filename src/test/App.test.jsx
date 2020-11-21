6ec8a05660559ca8855547bf0c49677f0e511c04
DevTobias <tobi.kaerst@gmx.de>
2020-11-21 21:02:45 +0100

    logo, coverage exludes

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -14,6 +14,6 @@ import '@testing-library/jest-dom';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Eunomia WG-Tracker/i);
   expect(linkElement).toBeInTheDocument();
 });
